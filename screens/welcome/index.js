40d10204af1d89cab2b604b15fb51920c57b0dd2
Ravi Reddy <ravikanthreddy.n@gmail.com>
2023-12-06 13:31:40 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { SafeAreaView, View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React from "react";
+import { SafeAreaView, View, Text, TextInput, Button, StyleSheet, Image } from "react-native";
 
 const SignupScreen = () => {
   return <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={{
+      uri: 'https://tinyurl.com/42evm3m3'
+    }} />
       <Text style={styles.title}>Sign Up</Text>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} placeholder="Username" />
+        <TextInput style={styles.input} placeholder="First Name" />
+        <TextInput style={styles.input} placeholder="Last Name" />
         <TextInput style={styles.input} placeholder="Email" keyboardType="email-address" />
         <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+        <TextInput style={styles.input} placeholder="Confirm Password" secureTextEntry />
         <Button title="Sign Up" />
+      </View>
+      <View style={styles.loginContainer}>
+        <Text style={styles.loginText}>Already have an account?</Text>
+        <Button title="Login" />
       </View>
     </SafeAreaView>;
 };
@@ -16,13 +25,19 @@ const SignupScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 16
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    alignSelf: "center",
+    marginBottom: 20
   },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginBottom: 24
   },
   inputContainer: {
@@ -30,10 +45,17 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 16,
     padding: 8
+  },
+  loginContainer: {
+    marginTop: 16
+  },
+  loginText: {
+    textAlign: "center",
+    marginBottom: 8
   }
 });
 export default SignupScreen;
